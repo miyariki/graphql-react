65311ab27ef153fae93df82126787f0649c4863a
宮北　力也 <miyakita@anconsulting.jp>
2021-12-17 13:27:38 +0900

    noname

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { ApolloProvider, Query} from 'react-apollo'
+import client from './client'
+import { SEARCH_REPOSITORIES } from './graphql'
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-console.log(GITHUB_TOKEN)
+const DEFAULT_STATE = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア"
+}
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = DEFAULT_STATE
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      ...DEFAULT_STATE,
+      query: event.target.value
+    })
+  }
+
+  render() {
+    const { query, first, last, before, after } = this.state
+    console.log({query})
+    return (
+      <ApolloProvider client={client}>
+        <form onSubmit={this.handleChange}>
+          <input value={query} onChange={this.handleChange} />
+        </form>
+
+        <Query query={SEARCH_REPOSITORIES} variables={{ query, first, last, before, after }}>
+          {
+            ({ loading, error, data }) => {
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
+              console.log(data)
+              return <div></div>
+            }
+          }
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
