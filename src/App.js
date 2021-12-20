83721f2772d3603bc04c28d0593da8c8163d6aae
宮北　力也 <miyakita@anconsulting.jp>
2021-12-20 16:29:51 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,27 +1,98 @@
 import React, { Component } from 'react';
-import { ApolloProvider, Query} from 'react-apollo'
+import { ApolloProvider, Query, Mutation} from 'react-apollo'
 import client from './client'
-import { SEARCH_REPOSITORIES } from './graphql'
+import { SEARCH_REPOSITORIES, ADD_STAR, REMOVE_STAR } from './graphql'
 
+const StarButton = props => {
+  const { node, query, first, last, before, after } = props
+  const totalCount = node.stargazers.totalCount
+  const viewerHasStarred = node.viewerHasStarred
+  const starCount = totalCount === 1 ? "1 star" : `${totalCount} stars`
+  const StarStatus = ({addOrRemoveStar}) => {
+    return(
+      <button onClick={
+        () => {
+          addOrRemoveStar({
+            variables: { input: { starrableId: node.id } },
+            update: (store, {data: {addStar, removeStar}}) => {
+
+              const { starrable } = addStar || removeStar
+              console.log(starrable)
+              const data = store.readQuery({
+                query: SEARCH_REPOSITORIES,
+                variables: { query, first, last, after, before }
+              })
+              const edges = data.search.edges
+              const newEdges = edges.map(edge => {
+                if (edge.node.id === node.id) {
+                  const totalCount = edge.node.stargazers.totalCount
+                  const diff = starrable.viewerHasStarred ? 1 : -1
+                  const newTotalCount = totalCount + diff
+                  edge.node.stargazers.totalCount = newTotalCount
+                }
+                return edge
+              })
+              data.search.edges = newEdges
+              store.writeQuery({ query: SEARCH_REPOSITORIES, data })
+            }
+          })
+        }
+      }>
+        {starCount} | {viewerHasStarred ? 'starred' : '-'}
+      </button>
+    )
+  }
+
+  return (
+    <Mutation
+      mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
+    >
+      {
+        addOrRemoveStar => <StarStatus addOrRemoveStar={addOrRemoveStar} />
+      }
+    </Mutation>
+  )
+}
+
+const PER_PAGE = 5
 const DEFAULT_STATE = {
-  first: 5,
+  first: PER_PAGE,
   after: null,
   last: null,
   before: null,
-  query: "フロントエンドエンジニア"
+  query: ""
 }
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = DEFAULT_STATE
-    this.handleChange = this.handleChange.bind(this)
+    this.myRef = React.createRef()
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange(event) {
+  handleSubmit(event) {
+    event.preventDefault()
+
     this.setState({
-      ...DEFAULT_STATE,
-      query: event.target.value
+      query: this.myRef.current.value
+    })
+  }
+
+  goPrevious(search) {
+    this.setState({
+      first: null,
+      after: null,
+      last: PER_PAGE,
+      before: search.pageInfo.startCursor,
+    })
+  }
+  goNext(search) {
+    this.setState({
+      first: PER_PAGE,
+      after: search.pageInfo.endCursor,
+      last: null,
+      before: null,
     })
   }
 
@@ -30,17 +101,59 @@ class App extends Component {
     console.log({query})
     return (
       <ApolloProvider client={client}>
-        <form onSubmit={this.handleChange}>
-          <input value={query} onChange={this.handleChange} />
+        <form onSubmit={this.handleSubmit}>
+          <input ref={this.myRef} />
+          <input type='submit' value='Submit' />
         </form>
 
-        <Query query={SEARCH_REPOSITORIES} variables={{ query, first, last, before, after }}>
+        <Query
+          query={SEARCH_REPOSITORIES}
+          variables={{ query, first, last, before, after }}
+        >
           {
             ({ loading, error, data }) => {
               if (loading) return 'Loading...'
               if (error) return `Error! ${error.message}`
-              console.log(data)
-              return <div></div>
+              const search = data.search
+              const repositoryCount = search.repositoryCount
+              const repositoryUnit = repositoryCount === 1? 'Repository' : 'Repositories'
+              const title = `GITHUB Repositories Search Results - ${data.search.repositoryCount} ${repositoryUnit}`
+              return (
+                <>
+                  <h2>{title}</h2>
+                  <ul>
+                    {
+                      search.edges.map(edge => {
+                        const node = edge.node
+                        return (
+                          <li key={node.id}>
+                            <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
+                            &nbsp;
+                            <StarButton node={node} {...{query, first, last, before, after}} />
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+
+                  {
+                    search.pageInfo.hasPreviousPage === true ?
+                      <button onClick={this.goPrevious.bind(this, search)}>
+                        Previous
+                      </button>
+                      :
+                      null
+                  }
+                  {
+                    search.pageInfo.hasNextPage === true ?
+                      <button onClick={this.goNext.bind(this, search)}>
+                        Next
+                      </button>
+                      :
+                      null
+                  }
+                </>
+              )
             }
           }
         </Query>
